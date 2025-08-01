ff95c1c4c7d43e115467202258cd131c4e1b7670
CyberAshu <mr.ayushsen@gmail.com>
2025-08-01 19:59:53 +0530

    update route

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,8 @@ from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    openapi_url="/v1/openapi.json",
+    root_path="/api"
 )
 
 # Set all CORS enabled origins
